912a601367ca6eff0c86a4829434868ea5e5d8ca
Seong Hyeon Kim <seong-ki@paul-f4Br2s1.clusters.42paris.fr>
2024-12-30 16:56:56 +0100

    Create t_tokenizer structure

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: seong-ki <seong-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:33:08 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/10/24 15:39:13 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/12/30 16:24:49 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 
 # include "builtin.h"
 
-t_token	*ft_new_token(char *value);
+typedef struct	s_tokenizer {
+	int	idx;
+	int	start;
+	int	is_first_token;
+	int	after_operator;
+	t_token	*tklst;
+} t_tokenizer;
+
+t_token	*ft_new_token(char *value, t_tokenizer *state);
 t_token	*ft_tklast(t_token *tklst);
 void	ft_token_add_back(t_token **tklst, t_token *new);
 void	change_env_vari(t_data *data, t_token **tklst);
