1eeaab06fb68498521c174139d3c223116be01c8
aejone <ohjjone1415@gmail.com>
2025-03-14 13:00:46 +0100

    builtins

---
+++ src/builtins/bi_utils.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaoh <jaoh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 18:02:54 by jaoh              #+#    #+#             */
+/*   Updated: 2025/03/14 12:44:58 by jaoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	bi_print_export(t_env *env)
+{
+	char	**envs;
+	char	*tmp;
+	char	*tmp_id;
+	int		i;
+
+	envs = ex_get_envs(env);
+	if (!envs)
+		return (1);
+	ft_advanced_sort_string_tab(envs, &ft_strcmp);
+	i = -1;
+	while (envs[++i])
+	{
+		printf("export ");
+		tmp = ft_strchr(envs[i], '=');
+		if (!tmp)
+			printf("%s\n", envs[i]);
+		else
+		{
+			tmp_id = env_create_id(envs[i]);
+			printf("%s=\"%s\"\n", tmp_id, tmp + 1);
+			free(tmp_id);
+		}
+	}
+	free(envs);
+	return (0);
+}
+
+static t_env	*bi_new_var(char *arg_id, char *arg_raw)
+{
+	t_env	*tmp;
+	char	*arg_value;
+
+	arg_value = env_create_value(arg_raw);
+	tmp = env_create(arg_id, arg_value, arg_raw);
+	if (!tmp)
+	{
+		free(arg_raw);
+		if (arg_value)
+			free(arg_value);
+		free(arg_id);
+		return (NULL);
+	}
+	return (tmp);
+}
+
+static void	bi_update_var(t_env *node, char *arg_raw)
+{
+	char	*arg_value;
+
+	arg_value = env_create_value(arg_raw);
+	if (!arg_value)
+	{
+		free(arg_raw);
+		return ;
+	}
+	if (node->value)
+		free(node->value);
+	if (node->raw)
+		free(node->raw);
+	node->value = arg_value;
+	node->raw = arg_raw;
+}
+
+int	bi_add_var(char *value, t_env **env)
+{
+	char	*arg_id;
+	char	*arg_raw;
+	t_env	*tmp;
+
+	arg_id = env_create_id(value);
+	if (!arg_id || !bi_check_id(arg_id))
+		return (free(arg_id), bi_err_export(value));
+	arg_raw = ft_strdup(value);
+	if (!arg_raw)
+		return (free(arg_id), 1);
+	tmp = ms_getenv(arg_id, *env);
+	if (!tmp)
+	{
+		tmp = bi_new_var(arg_id, arg_raw);
+		if (!tmp)
+			return (1);
+		env_add_back(env, tmp);
+	}
+	else
+	{
+		free(arg_id);
+		bi_update_var(tmp, arg_raw);
+	}
+	return (0);
+}
+
+int	bi_delete_var(char *value, t_env **env)
+{
+	t_env	*tmp;
+	t_env	*tmp2;
+
+	tmp = ms_getenv(value, *env);
+	if (!tmp)
+		return (0);
+	if (*env == tmp)
+		*env = tmp->next;
+	else
+	{
+		tmp2 = *env;
+		while (tmp2->next != tmp)
+			tmp2 = tmp2->next;
+		tmp2->next = tmp->next;
+	}
+	env_del_one(tmp);
+	return (0);
+}
+
+int	bi_nflag(char *flag)
+{
+	if (!ft_strncmp(flag, "-n", 2))
+		flag += 2;
+	else
+		return (1);
+	while (*flag)
+	{
+		if (*flag++ != 'n')
+			return (1);
+	}
+	return (0);
+}
---
+++ src/builtins/builtin.c
@@ -0,0 +1,191 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaoh <jaoh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 16:48:24 by jaoh              #+#    #+#             */
+/*   Updated: 2025/03/14 13:00:29 by jaoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	bi_is_builtin(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	if (!ft_strcmp(cmd, "echo") || !ft_strcmp(cmd, "cd") ||
+		!ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "export") ||
+		!ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "env"))
+		return (1);
+	if (!ft_strcmp(cmd, "exit"))
+		return (2);
+	return (0);
+}
+
+
+int	bi_do_builtin(t_data *data, char *cmd, t_args *args)
+{
+	if (!ft_strcmp(cmd, "echo"))
+		return (bi_echo(args));
+	/*if (!ft_strcmp(cmd, "cd"))
+		return (bi_cd(data, args));
+	if (!ft_strcmp(cmd, "pwd"))
+		return (bi_pwd(args));*/
+	if (!ft_strcmp(cmd, "export"))
+		return (bi_export(data, args));
+	if (!ft_strcmp(cmd, "unset"))
+		return (bi_unset(data, args));
+	if (!ft_strcmp(cmd, "env"))
+		return (bi_env(data, args));
+	if (!ft_strcmp(cmd, "exit"))
+		return (bi_exit(data, args));
+	return (0);
+}
+
+int	bi_echo(t_args *args)
+{
+	int	n_flag;
+
+	n_flag = 0;
+	while (args && !bi_nflag(args->value))
+	{
+		n_flag = 1;
+		args = args->next;
+	}
+	while (args)
+	{
+		printf("%s", args->value);
+		if (args->next)
+			printf("%s", " ");
+		args = args->next;
+	}
+	if (!n_flag)
+		printf("%s", "\n");
+	return (0);
+}
+
+int	bi_cd(t_data *data, t_args *args)
+{
+	int		ac;
+	char	*cwd;
+	t_env	*home;
+
+	ac = arg_lstsize(args);
+	if (ac > 1)
+		return (ft_putstr_fd("minishell: cd: too many arguments\n",
+				STDERR_FILENO), 1);
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		perror("minishell: cd: error no current directory");
+	home = ms_getenv("HOME", data->envp);
+	if ((!ac || !ft_strcmp(args->value, "--")) && home && home->value)
+		chdir(home->value);
+	else if ((!ac || !ft_strcmp(args->value, "--")) && (!home || !home->value))
+		return (bi_err_cd(errno, "HOME"), free(cwd), 1);
+	else if (chdir(args->value) < 0)
+	{
+		bi_err_cd(errno, args->value);
+		return (free(cwd), 1);
+	}
+	if (bi_update_pwd(data, cwd))
+		return (free(cwd), 1);
+	return (free(cwd), 0);
+}
+
+int	bi_pwd(t_args *args)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("minishell: pwd: error no current directory");
+		return (1);
+	}
+	printf("%s\n", cwd);
+	free(cwd);
+	return (0);
+}
+
+int	bi_export(t_data *data, t_args *args)
+{
+	int	exit_code;
+
+	exit_code = 0;
+	if (!args)
+	{
+		if (bi_print_export(data->envp))
+			exit_code = 1;
+	}
+	else
+	{
+		while (args)
+		{
+			if (bi_add_var(args->value, &(data->envp)))
+				exit_code = 1;
+			args = args->next;
+		}
+	}
+	return (exit_code);
+}
+
+int	bi_unset(t_data *data, t_args *args)
+{
+	if (!args)
+		return (0);
+	else
+	{
+		while (args)
+		{
+			if (bi_delete_var(args->value, &(data->envp)))
+				return (1);
+			args = args->next;
+		}
+	}
+	return (0);
+}
+
+int	bi_env(t_data *data, t_args *args)
+{
+	t_env	*tmp;
+
+	tmp = data->envp;
+	if (args)
+	{
+		bi_err_env(args->value);
+		return (127);
+	}
+	while (tmp)
+	{
+		if (tmp->value)
+			printf("%s=%s\n", tmp->id, tmp->value);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+// 인자가 숫자가 아니면 오류 처리
+// 인자가 두 개 이상이면 오류 처리
+// 정상적으로 종료 코드 설정 후 종료
+int	bi_exit(t_data *data, t_args *args)
+{
+	int		exit_code;
+
+	if (args && args->next && !bi_check_exitcode(args->value))
+		return (ft_putstr_fd("minishell: exit: too many arguments\n",
+				STDERR_FILENO), 1);
+	exit_code = 0;
+	if (args && !bi_check_exitcode(args->value))
+		exit_code = ft_atoi(args->value);
+	else if (args && bi_check_exitcode(args->value))
+	{
+		bi_err_exit(args->value);
+		exit_code = 2;
+	}
+	ex_close_all_fds(data, NULL);
+	ms_free_all(data);
+	exit(exit_code);
+}
